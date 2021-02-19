c5fbeda164712c8560c73d6b5202e34882bb1364
jagat-barua <jagat.barua@gmail.com>
2021-02-19 19:44:12 +0600

    Add files via upload

---
+++ Utility/ConnectionString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRecordManagementSystem.Utility
+{
+    public class ConnectionString
+    {
+        private static string cName = "Data Source=.; Initial Catalog=StudentManagement;User ID=sa;Password=123";
+        public static string CName
+        {
+            get => cName;
+        }
+
+    }
+}
